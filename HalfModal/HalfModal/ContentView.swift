36ec287703443c66734ff18af47f879139d9799c
岡優志 <yu.j.0513@gmail.com>
2021-12-21 14:50:27 +0900

    -

--- HalfModal/HalfModal/ContentView.swift
+++ HalfModal/HalfModal/ContentView.swift
@@ -8,10 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show = false
+    @State var bottomState = CGSize.zero
+    @State var showFull = false
+    @State var showMenu = false
     var body: some View {
-        ZStack {
-        ModalView()
-            
+        VStack {
+            Button {
+                show.toggle()
+            } label: {
+                Text("ボタン")
+            }
+
+            ZStack {
+                ModalView()
+                    .offset(y:show ? 360 : 2000)
+                    .offset(y: bottomState.height)
+                    .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+                
+                    .gesture(
+                        DragGesture().onChanged { value in
+                            self.bottomState = value.translation
+                            if self.showFull {
+                                self.bottomState.height += -300
+                            }
+                            if self.bottomState.height < -300 {
+                                self.bottomState.height = -300
+                            }
+                        }
+                            .onEnded { value in
+                                if self.bottomState.height > 50 {
+                                    self.show = false
+                                    self.showMenu = false
+                                }
+                                if (self.bottomState.height < -100 && !self.showFull) || (self.bottomState.height < -250 && self.showFull) {
+                                    self.bottomState.height = -300
+                                    self.showFull = true
+                                } else {
+                                    self.bottomState = .zero
+                                    self.showFull = false
+                                    
+                                }
+                            }
+                    )
+            }
         }
     }
 }
