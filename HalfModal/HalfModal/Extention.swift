cacadd5714455f09cb997bf82b25bc78e1cc80a4
岡優志 <yu.j.0513@gmail.com>
2021-12-22 14:13:26 +0900

    -

--- HalfModal/HalfModal/Extention.swift
+++ HalfModal/HalfModal/Extention.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct CustomModalModifier: ViewModifier {
+    @State var show = false
+    @State var showFull = false
+    @State var showMenu = false
+    @State var bottomState = CGSize.zero
+    @State var amount = UIScreen.main.bounds
     func body(content: Content) -> some View {
         content
-            .disableAutocorrection(true)
-            .autocapitalization(.none)
-            .keyboardType(.URL)
+            .cornerRadius(20)
+            .offset(y:show ? amount.height * 0.4 : amount.height)
+            .offset(y: bottomState.height)
+            .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.9), value: show)
+        
+            .gesture(
+                DragGesture().onChanged { value in
+                    self.bottomState = value.translation
+                    if self.showFull {
+                        self.bottomState.height += -300
+                    }
+                    if self.bottomState.height < -300 {
+                        self.bottomState.height = -300
+                    }
+                }
+                    .onEnded { value in
+                        if self.bottomState.height > 50 {
+                            self.show = false
+                           // self.showMenu = false
+                        }
+                        if (self.bottomState.height < -100 && !self.showFull) || (self.bottomState.height < -250 && self.showFull) {
+                            self.bottomState.height = -300
+                            self.showFull = true
+                        } else {
+                            self.bottomState = .zero
+                            self.showFull = false
+                            
+                        }
+                    }
+            )
+
     }
 }
 
